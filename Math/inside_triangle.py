ef5d8c2d145a0ae0e04fa157fff2271dbffc2c1f
mdallow97 <mikedallow@gmail.com>
2021-03-05 14:10:44 -0800

    Uploaded a solution to finding if a point is in a triangle

---
+++ Math/inside_triangle.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def inside_triangle(triangle, point):
+	def triangle_area(sub_triangle):
+		v1 = sub_triangle[1] - sub_triangle[0]
+		v2 = sub_triangle[2] - sub_triangle[0]
+		return abs(np.linalg.det([v1]+[v2])) / 2
+
+	total_area = triangle_area(triangle)
+	print([point, triangle[0], triangle[1]])
+	sub1 = triangle_area([point, triangle[0], triangle[1]])
+	sub2 = triangle_area([point, triangle[1], triangle[2]])
+	sub3 = triangle_area([point, triangle[0], triangle[2]])
+
+	if total_area == sub1 + sub2 + sub3:
+		return True
+	else:
+		return False
+
+
+def main():
+	triangle = np.array([[0,1], [4,1], [2,2]])
+	point = np.array([2,1.5])
+
+	print(inside_triangle(triangle, point))
+
+if __name__ == '__main__':
+	main()
